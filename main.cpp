8b959d9c69612e205f17242d67b28af20a642d43
Alan Cortez <acortez1337@csu.fullerton.edu>
2023-11-27 17:47:54 -0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <map>
+#include <string>
+
+int main()
+{
+    std::vector < std::vector<std::map<std::string, std::size_t>> graph = /* TODO initialize the graph */;
+
+    // ⭐TODO: create an insert function to add a new vertex
+
+    // ⭐TODO: create a function to update the edge weight of two points
+
+    // ⭐TODO: create a function that returns the number of the amount of children a node has
+
+    // ⭐TODO: create a function that returns the edge weight of the node to another node (so like what is the distance from point A to point B)
+
+    return 0;
+}
